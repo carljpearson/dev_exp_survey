5fc878d8d0a66be655b312149e0a8cb66ef17b7c
carljpearson <carl.j.pearson@gmail.com>
2019-10-03 15:41:45 -0500

    Read in data

---
+++ Cleaning.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(googlesheets)
+
+df18 <- read_csv("/Users/carlpearson/Documents/data_internal/dev_exp/dev_exp_survey_2018_10_06.csv",col_names = T)
+
+df19 <- read_csv("/Users/carlpearson/Documents/data_internal/dev_exp/dev_exp_survey_early_2019_10_3.csv",col_names = T)
